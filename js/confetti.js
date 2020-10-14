6210f99b07a2e23c1cc19368b5e89506f1d74ef3
Aditi Sharma <aditisharma@Aditis-MacBook-Pro.local>
2020-10-15 00:07:45 +0530

    Confetti

--- js/confetti.js
+++ js/confetti.js
@@ -1,3 +1,7 @@
-var confettiSettings = { target: 'my-canvas' };
+//<script src="node_modules/confetti-js/dist/index.min.js"></script>
+import { ConfettiGenerator } from "confetti-js";
+
+var confettiElement = document.getElementById('my-canvas');
+var confettiSettings = {"target": confettiElement,"max":"80","size":"1","animate":true,"props":["circle","square","triangle","line",{"type":"svg","src":"site/hat.svg","size":25,"weight":0.2}],"colors":[[165,104,246],[230,61,135],[0,199,228],[253,214,126]],"clock":"25","rotate":false,"width":"1440","height":"748","start_from_edge":false,"respawn":true};
 var confetti = new ConfettiGenerator(confettiSettings);
 confetti.render();
