99d9fb014ac4b23d5a44e1aff6a3a97d22dae21c
Aditi Sharma <aditisharma@Aditis-MacBook-Pro.local>
2020-10-17 21:25:27 +0530

    Updates

---
+++ precache-manifest.076bbeadd7c1564be177d2c548b3a73d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0751779b64ce77e3bf8d1cb4377eb86c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49a095f9c7c26d1fcfc6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3883b9c1ca5f497645fb",
+    "url": "/static/js/2.25474064.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.25474064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a095f9c7c26d1fcfc6",
+    "url": "/static/js/main.c141c94e.chunk.js"
+  },
+  {
+    "revision": "e89ab622580c630ffbf5",
+    "url": "/static/js/runtime-main.494bf30b.js"
+  }
+]);
